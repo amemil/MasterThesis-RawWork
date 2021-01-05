2670e73ef620b6135a091f299ae925abc0e7bb83
Emil Alvar Myhre <emilam@Emils-MacBook-Air.local>
2021-01-05 23:23:58 +0100

    :D

--- DatasizeAnalysis/Datasizes.py
+++ DatasizeAnalysis/Datasizes.py
@@ -5,14 +5,14 @@ Created on Tue Jan  5 21:16:26 2021
 
 @author: emilam
 """
-import numpy as np              
-import matplotlib.pyplot as plt 
-from tqdm import tqdm
-from scipy.stats import gamma
-from numba import njit
-@njit
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import UtilitiesMaster as ut
 
-print(1)
+
+data = ut.SimulatedData(Ap=0.005, tau=0.02, std=0.001,b1=-2.0, b2=-2.0, w0=1.0)
+data.create_data()
+s1,s2,t,W = data.get_data()
 
 #Affect of the datasize (longer time scale), 2 noise levels -> check for bigger time domains
 #Decorrelation of A and Tau with bigger datasets (not learning rule shape if more data)
--- UtilitiesMaster.py
+++ UtilitiesMaster.py
@@ -298,16 +298,17 @@ class ParameterInference():
             theta_prior = np.copy(theta_choice)
             old_log_post = [np.copy(old_log_post),np.copy(new_log_post)][choice == 1]
         return theta
-        
-data = SimulatedData(Ap=0.005, tau=0.02, std=0.001,b1=-2.0, b2=-2.0, w0=1.0)
-data.create_data()
-s1,s2,t,W = data.get_data()
+
+if __name__ == "__main__":  
+    data = SimulatedData(Ap=0.005, tau=0.02, std=0.001,b1=-2.0, b2=-2.0, w0=1.0)
+    data.create_data()
+    s1,s2,t,W = data.get_data()
 
 
-inference = ParameterInference(s1, s2,P = 1000, Usim = 100, Ualt = 200, it = 1500, std=0.0001, N = 2\
-                               ,shapes_prior = np.array([4,5]), rates_prior = np.array([50,100]))
-b1est = inference.b1_estimation()
-b2est,w0est = inference.b2_w0_estimation()
+    inference = ParameterInference(s1, s2,P = 1000, Usim = 100, Ualt = 200, it = 1500, std=0.0001, N = 2\
+                                   ,shapes_prior = np.array([4,5]), rates_prior = np.array([50,100]))
+    b1est = inference.b1_estimation()
+    b2est,w0est = inference.b2_w0_estimation()
 
-theta_sim = inference.standardMH()
-theta_alt = inference.alternatingMH()
+    theta_sim = inference.standardMH()
+    theta_alt = inference.alternatingMH()
