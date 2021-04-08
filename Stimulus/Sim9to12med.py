1a7dce893487555ae1c20a1877fc608ca2abd816
Emil Myhre <emilalvar.myhre@gmail.com>
2021-04-08 23:28:48 +0200

    datasizesSim

--- Stimulus/Sim9to12med.py
+++ Stimulus/Sim9to12med.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan 29 12:25:25 2021
+Created on Thu Apr  8 16:37:19 2021
 
 @author: emilam
 """
@@ -14,20 +14,17 @@ import UtilitiesMaster as ut
 datasizes = [60,120,180,240,300] #seconds
 
 #ONLY A, 2ms binsize, different datasizes
-
+data = np.load('taudata9to12stim.npy')
 samples = []
 for i in range(4):
-    w0est1 = -np.inf
     samples_temp = [] 
-    while (w0est1 < 0.97 or w0est1 > 1.03):
-        data = ut.SimulatedData(Ap=0.005, tau=0.02, std=0.0001,b1=-3.1, b2=-3.1, w0=1.0,sec = datasizes[-1], binsize = 1/500.0)
-        data.create_data()
-        s1,s2,t,W = data.get_data()
-        inference = ut.ParameterInference(s1,s2,P = 100, Usim = 100, Ualt = 200,it = 1500, infstd=0.0001, N = 2\
-             , shapes_prior = np.array([4,5]), rates_prior = np.array([50,100]),sec=datasizes[-1]\
-                 ,binsize = 1/500.0)
-        b1est1 = inference.b1_estimation()
-        b2est1,w0est1 = inference.b2_w0_estimation()
+    s1 = data[i][0][:]
+    s2 = data[i][1][:]
+    inference = ut.ParameterInference(s1,s2,P = 100, Usim = 100, Ualt = 200,it = 1500, infstd=0.0001, N = 2\
+         , shapes_prior = np.array([4,5]), rates_prior = np.array([50,100]),sec=datasizes[-1]\
+             ,binsize = 1/500.0)
+    b1est1 = inference.b1_estimation()
+    b2est1,w0est1 = inference.b2_w0_estimation()
     for j in range(len(datasizes)):
         s1_sub = s1[:int(len(s1)*(datasizes[j]/datasizes[-1]))]
         s2_sub = s2[:int(len(s2)*(datasizes[j]/datasizes[-1]))]
@@ -41,4 +38,4 @@ for i in range(4):
     samples.append(samples_temp)
     
 
-np.save('SimSamples1to4',samples)
+np.save('SimSamples9to12medstim',samples)
--- Stimulus/sim13to16mhfreq.py
+++ Stimulus/sim13to16mhfreq.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan 29 12:25:25 2021
+Created on Thu Apr  8 23:23:09 2021
 
 @author: emilam
 """
@@ -13,32 +13,41 @@ import UtilitiesMaster as ut
 
 datasizes = [60,120,180,240,300] #seconds
 
-#ONLY A, 2ms binsize, different datasizes
+#sim, 2ms binsize, different datasizes, freq stim
 
 samples = []
+datasets = []
 for i in range(4):
+    count = 0
     w0est1 = -np.inf
     samples_temp = [] 
     while (w0est1 < 0.97 or w0est1 > 1.03):
-        data = ut.SimulatedData(Ap=0.005, tau=0.02, std=0.0001,b1=-3.1, b2=-3.1, w0=1.0,sec = datasizes[-1], binsize = 1/500.0)
-        data.create_data()
+        data = ut.SimulatedData(Ap=0.005, tau=0.02, std=0.0001,b1=-3.1, b2=-3.1, w0=1.0,sec = datasizes[-1], binsize = 1/500.0,freq = 200)
+        data.create_freq_data()
         s1,s2,t,W = data.get_data()
         inference = ut.ParameterInference(s1,s2,P = 100, Usim = 100, Ualt = 200,it = 1500, infstd=0.0001, N = 2\
              , shapes_prior = np.array([4,5]), rates_prior = np.array([50,100]),sec=datasizes[-1]\
-                 ,binsize = 1/500.0)
+                 ,binsize = 1/500.0,Afix = 0.005)
         b1est1 = inference.b1_estimation()
         b2est1,w0est1 = inference.b2_w0_estimation()
+        count += 1
+        if count > 30:
+            w0est1 = 1
+    datasets_temp = [s1,s2]
+    datasets.append(datasets_temp)
     for j in range(len(datasizes)):
         s1_sub = s1[:int(len(s1)*(datasizes[j]/datasizes[-1]))]
         s2_sub = s2[:int(len(s2)*(datasizes[j]/datasizes[-1]))]
         inference_sub = ut.ParameterInference(s1_sub,s2_sub,P = 100, Usim = 100, Ualt = 200,it = 1500, infstd=0.0001, N = 2\
              , shapes_prior = np.array([4,5]), rates_prior = np.array([50,100]),sec=datasizes[j]\
-                 ,binsize = 1/500.0)
+                 ,binsize = 1/500.0,Afix = 0.005)
         b1est1_sub = inference_sub.b1_estimation()
         b2est1_sub,w0est1_sub = inference_sub.b2_w0_estimation()
-        Tauest = inference_sub.standardMH() 
-        samples_temp.append(Tauest)
+        est = inference_sub.standardMH() 
+        samples_temp.append(est)
     samples.append(samples_temp)
     
 
-np.save('SimSamples1to4',samples)
+np.save('Samples13to16megahighfreq',samples)
+#np.save('secondsss',datasizes)
+np.save('data13to16megahighfreq',datasets)
