53b978aa176a9dbf946bf03d20d386b835ed7af5
Emil Myhre <36158578+amemil@users.noreply.github.com>
2021-05-15 21:53:16 +0200

    Update LargeScaleOptim8.py

--- FullDesign/LargeScaleOptim8.py
+++ FullDesign/LargeScaleOptim8.py
@@ -15,14 +15,14 @@ s1init = np.load('s1init_8.npy')
 s2init = np.load('s2init_8.npy')
 Winit = np.load('Winit_8.npy')
 
-design = ut.ExperimentDesign(freqs_init=np.array([20,50,100,200]),maxtime=60,trialsize=5\
+design = ut.ExperimentDesign(freqs_init=np.array([10,20,50,100]),maxtime=60,trialsize=5\
                  ,Ap=0.005, tau=0.02, genstd=0.0001,b1=-3.1, b2=-3.1, w0=1.0,binsize = 1/500.0,reals = 15,longinit = 60\
                      ,s1init = s1init,s2init = s2init,Winit = Winit)
 means,entrs,optms,mutinfs,W,posts = design.onlineDesign_wh(nofreq =False,constant = False, random = False, optimised = True)
 
-np.save('WHestmimates_8',means)
-np.save('WHentropies_8',entrs)
-np.save('WHoptfrqs_8',optms)
-np.save('WHmutinfs_8',mutinfs)
-np.save('WHw_8',W)
-np.save('WHposts_8',posts)
+np.save('WHestmimatesNewGrid_8',means)
+np.save('WHentropiesNewGrid_8',entrs)
+np.save('WHoptfrqsNewGrid_8',optms)
+np.save('WHmutinfsNewGrid_8',mutinfs)
+np.save('WHwNewGrid_8',W)
+np.save('WHpostsNewGrid_8',posts)
