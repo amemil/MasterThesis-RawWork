56d3d832e0bccbffee25289f032d1f81e89ab5c7
Emil Myhre <36158578+amemil@users.noreply.github.com>
2021-05-15 22:03:39 +0200

    Update LargeScaleOptim19.py

--- FullDesign/LargeScaleOptim19.py
+++ FullDesign/LargeScaleOptim19.py
@@ -15,14 +15,14 @@ s1init = np.load('s1init_19.npy')
 s2init = np.load('s2init_19.npy')
 Winit = np.load('Winit_19.npy')
 
-design = ut.ExperimentDesign(freqs_init=np.array([20,50,100,200]),maxtime=60,trialsize=5\
+design = ut.ExperimentDesign(freqs_init=np.array([10,20,50,100]),maxtime=60,trialsize=5\
                  ,Ap=0.005, tau=0.02, genstd=0.0001,b1=-3.1, b2=-3.1, w0=1.0,binsize = 1/500.0,reals = 15,longinit = 60\
                      ,s1init = s1init,s2init = s2init,Winit = Winit)
 means,entrs,optms,mutinfs,W,posts = design.onlineDesign_wh(nofreq =False,constant = False, random = False, optimised = True)
 
-np.save('WHestmimates_19',means)
-np.save('WHentropies_19',entrs)
-np.save('WHoptfrqs_19',optms)
-np.save('WHmutinfs_19',mutinfs)
-np.save('WHw_19',W)
-np.save('WHposts_19',posts)
+np.save('WHestmimatesNewGrid_19',means)
+np.save('WHentropiesNewGrid_19',entrs)
+np.save('WHoptfrqsNewGrid_19',optms)
+np.save('WHmutinfsNewGrid_19',mutinfs)
+np.save('WHwNewGrid_19',W)
+np.save('WHpostsNewGrid_19',posts)
