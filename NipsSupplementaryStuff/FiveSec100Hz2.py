c6903134d01675e0ab1ada0737e10b58afcbc565
Emil Myhre <36158578+amemil@users.noreply.github.com>
2021-06-01 19:31:55 +0200

    Update FiveSec100Hz2.py

--- NipsSupplementaryStuff/FiveSec100Hz2.py
+++ NipsSupplementaryStuff/FiveSec100Hz2.py
@@ -18,14 +18,14 @@ samples = []
 for i in range(10):
     samples_temp = []
     for j in range(len(w0s)):
-        data = ut.SimulatedData(Ap=0.005, tau=0.02, std=0.0001,b1=-3.1, b2=-3.1, w0=w0s[j],sec = 5, binsize = 1/500.0,freq = 100)
+        data = ut.SimulatedData(Ap=0.005, tau=0.005, std=0.0001,b1=-3.1, b2=-3.1, w0=w0s[j],sec = 5, binsize = 1/500.0,freq = 100)
         data.create_freq_data()
         s1,s2,_,W = data.get_data()
         infer= ut.ParameterInference(s1,s2,P = 50, Usim = 100, Ualt = 200,it = 1500, infstd=0.0001, N = 2\
                                      , shapes_prior = np.array([4,5]), rates_prior = np.array([50,100]),sec=5\
-                                         ,binsize = 1/500.0,taufix = 0.02,Afix = 0.005,b1est = -3.1,b2est = -3.1,w0est = w0s[j])
+                                         ,binsize = 1/500.0,taufix = 0.005,Afix = 0.005,b1est = -3.1,b2est = -3.1,w0est = w0s[j])
         sample = infer.standardMH()
         samples_temp.append(sample)
     samples.append(samples_temp)
     
-np.save('Samples5sec100Hz11to20',samples)
+np.save('Samples5sec100Hz11to20_Lr2',samples)
