f45c85877c088d3c1790928aded2d94aac566093
Emil Myhre <emilalvar.myhre@gmail.com>
2021-06-15 18:15:55 +0200

    upd

--- Stimulus/A17to20stim.py
+++ Stimulus/A17to20stim.py
@@ -17,14 +17,14 @@ datasizes = [60,120,180,240,300] #seconds
 
 samples = []
 datasets = []
-for i in range(4):
+for i in range(2):
     w0est1 = -np.inf
     samples_temp = [] 
     while (w0est1 < 0.97 or w0est1 > 1.03):
         data = ut.SimulatedData(Ap=0.005, tau=0.02, std=0.0001,b1=-1.5, b2=-3.1, w0=1.0,sec = datasizes[-1], binsize = 1/500.0)
         data.create_data()
         s1,s2,t,W = data.get_data()
-        inference = ut.ParameterInference(s1,s2,P = 200, Usim = 100, Ualt = 200,it = 1500, std=0.0001, N = 1\
+        inference = ut.ParameterInference(s1,s2,P = 50, Usim = 100, Ualt = 200,it = 1500, std=0.0001, N = 1\
              , shapes_prior = np.array([4]), rates_prior = np.array([50]),sec=datasizes[-1]\
                  ,binsize = 1/500.0,taufix = 0.02)
         b1est1 = inference.b1_estimation()
@@ -34,7 +34,7 @@ for i in range(4):
     for j in range(len(datasizes)):
         s1_sub = s1[:int(len(s1)*(datasizes[j]/datasizes[-1]))]
         s2_sub = s2[:int(len(s2)*(datasizes[j]/datasizes[-1]))]
-        inference_sub = ut.ParameterInference(s1_sub,s2_sub,P = 200, Usim = 100, Ualt = 200,it = 1500, std=0.0001, N = 1\
+        inference_sub = ut.ParameterInference(s1_sub,s2_sub,P = 50, Usim = 100, Ualt = 200,it = 1500, std=0.0001, N = 1\
              , shapes_prior = np.array([4]), rates_prior = np.array([50]),sec=datasizes[j]\
                  ,binsize = 1/500.0,taufix = 0.02)
         b1est1_sub = inference_sub.b1_estimation()
@@ -44,6 +44,6 @@ for i in range(4):
     samples.append(samples_temp)
     
 
-np.save('ASamples17to20stim',samples)
+np.save('ASamples9to10stim',samples)
 #np.save('secondsss',datasizes)
-np.save('data17to20stim',datasets)
+#np.save('data17to20stim',datasets)
